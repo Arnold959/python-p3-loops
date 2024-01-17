22cd99219c54c9c150e05f466838d2b67bfa1a9c
arnold <musaliokello@gmail.com>
2024-01-18 01:07:49 +0300

    Changes

--- lib/looping.py
+++ lib/looping.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
-
 def happy_new_year():
-    count = 10
-    while count >= 1:
-        print (count)
-        count -= 1
-    print ("Happy New Year!")
-    pass
-
-def square_integers(int_list):
-    square_list =[num**2 for num in int_list]
-    return square_list
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
 
 def fizzbuzz():
-    for num in range (1, 101):
-     if (num % 3 == 0 and num % 5 == 0):
-        print ("FizzBuzz")
-     elif (num % 3 == 0):
-        print ("Fizz")
-     elif (num % 5 == 0):
-        print ("Buzz")
-     else:       
-        print (num)
-    
-    pass
+   for  num in range (1,101):
+       if num % 3 == 0 and num % 5 == 0:
+           print ("FizzBuzz")
+       elif num % 3 == 0:
+           print ("Fizz")
+       elif num % 5 == 0:
+           print ( "Buzz")
+       else :
+           print (num)
+
+def square_integers (int_list):
+    square_list = [num**2 for num in int_list]
+    return square_list
+
+def reverse_string(input_str):
+    reversed_str = ""
+    for char in input_str:
+        reversed_str = char + reversed_str
+    return reversed_str
+
+
